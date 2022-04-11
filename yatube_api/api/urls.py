254219ac04ec2a8136021862128a0f73370ef327
Viacheslav Ivanov <viachmerc@gmail.com>
2022-04-11 22:07:59 +0300

    first commit

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,24 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+from .views import CommentViewSet, GroupViewSet, PostViewSet
+
+app_name = 'api'
+
+router = routers.DefaultRouter()
+
+router.register('posts', PostViewSet, basename='posts')
+router.register('groups', GroupViewSet, basename='groups')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+
 
 urlpatterns = [
+    path('v1/auth/', include('djoser.urls')),
+    path('v1/auth/', include('djoser.urls.jwt')),
+    path('v1/', include(router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,40 @@
-# TODO:  Напишите свой вариант
+from rest_framework import permissions, viewsets
+from rest_framework.generics import get_object_or_404
+
+from posts.models import Comment, Group, Post
+
+from .permissions import IsAuthorOrReadOnly
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticated, IsAuthorOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticated, IsAuthorOrReadOnly)
+
+    def _get_post(self):
+        return get_object_or_404(Post, id=self.kwargs.get('post_id'))
+
+    def get_queryset(self):
+        return self._get_post().comments
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post=self._get_post(),
+        )
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
